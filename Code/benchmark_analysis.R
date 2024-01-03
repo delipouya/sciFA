d2c6316b842d6695715b2c3cf92b2495afdc9309
delaram-pouya <d.pouyabahar@gmail.com>
2024-01-03 12:56:19 -0500

    calculate pvalues for residual/mean/scale calculations

--- Code/benchmark_analysis.R
+++ Code/benchmark_analysis.R
@@ -1,18 +1,8 @@
 library(ggplot2)
 library(ggpubr)
 library(reshape2)
-scale_minMax <- function(x){
-  x_min = min(x)
-  x_max = max(x)
-  scaled = (x-x_min)/(x_max-x_min)
-  return(scaled)
-}
+library(ggplot2)
 
-scale_Max <- function(x){
-  x_max = max(x)
-  scaled = (x)/(x_max)
-  return(scaled)
-}
 ###############################################################################################
 ########################## importance evaluation for model comparison
 ################################################################################################
@@ -49,26 +39,115 @@ head(imp_shuffle_deviance)
 dim(imp_shuffle_deviance)
 
 importance_df_residual_shuffle = rbind(rbind(imp_shuffle_pearson, imp_shuffle_response)) #imp_shuffle_deviance
-head(importance_df_residual_shuffle)
 importance_df_residual_shuffle$type = 'shuffle'
+head(importance_df_residual_shuffle)
 
-library(ggplot2)
+
+
+################## Some visualization of the results ################## 
 importance_df_residual_merged = rbind(importance_df_residual_baseline, importance_df_residual_shuffle)
 importance_df_residual_merged$importance_abs = abs(importance_df_residual_merged$importance)
+
 ggplot(importance_df_residual_merged, aes(y=importance_abs, x=model, fill=type))+geom_boxplot()+theme_classic()+
   coord_flip()+ggtitle('model comparison pearson and response combined')
-
 ggplot(importance_df_residual_merged[importance_df_residual_merged$residual_type=='pearson',], 
        aes(y=importance_abs, x=model, fill=type))+geom_boxplot()+theme_classic()+
   coord_flip()+ggtitle('model comparison pearson ')
-
 ggplot(importance_df_residual_merged[importance_df_residual_merged$residual_type=='response',], 
        aes(y=importance_abs, x=model, fill=type))+geom_boxplot()+theme_classic()+
   coord_flip()+ggtitle('model comparison response ')
-
 ggplot(importance_df_residual_merged, aes(y=importance_abs, x=res, fill=type))+geom_boxplot()+theme_classic()+coord_flip()
 
 head(importance_df_residual_merged)
+########################################################################
+
+########## splitting the baseline data frames
+table(importance_df_residual_baseline$residual_type)
+table(importance_df_residual_baseline$model)
+
+residual_type_names = names(table(importance_df_residual_baseline$residual_type))
+model_type_names = names(table(importance_df_residual_baseline$model))
+
+for (residual_type in residual_type_names){
+  res_df = importance_df_residual_baseline[importance_df_residual_baseline$residual_type==residual_type,]
+  for (model_type in model_type_names){
+    res_df_model = res_df[res_df$model==model_type,]
+    print(head(res_df_model, 3))
+    print(paste0(residual_type, '_', model_type))
+    write.csv(res_df_model,paste0('~/sciFA/Results/benchmark/analysis/imp/baseline_df_',residual_type, '_', model_type,'.csv'))
+  }
+}
+
+########################################
+########## splitting the shuffled data frames
+
+table(importance_df_residual_shuffle$residual_type)
+table(importance_df_residual_shuffle$model)
+
+residual_type_names = names(table(importance_df_residual_shuffle$residual_type))
+model_type_names = names(table(importance_df_residual_shuffle$model))
+
+for (residual_type in residual_type_names){
+  res_df = importance_df_residual_shuffle[importance_df_residual_shuffle$residual_type==residual_type,]
+  for (model_type in model_type_names){
+    res_df_model = res_df[res_df$model==model_type,]
+    print(head(res_df_model, 3))
+    print(paste0(residual_type, '_', model_type))
+    write.csv(res_df_model,paste0('~/sciFA/Results/benchmark/analysis/imp/shuffle_df_',residual_type, '_', model_type,'.csv'))
+  }
+}
+########################################
+
+thr = 0.05
+cov_level_names =c("b'CELseq2'", "b'Dropseq'", "b'sc_10X'", "H1975","H2228","HCC827" )
+summary_df = data.frame(matrix(nrow = length(residual_type_names)*length(model_type_names),ncol = length(cov_level_names)))
+colnames(summary_df) = cov_level_names
+
+row=1
+for (residual_type in residual_type_names){
+  for (model_type in model_type_names){
+
+    basline_df = read.csv(paste0('~/sciFA/Results/benchmark/analysis/imp/baseline_df_',residual_type, '_', model_type,'.csv'))
+    shuffle_df = read.csv(paste0('~/sciFA/Results/benchmark/analysis/imp/shuffle_df_',residual_type, '_', model_type,'.csv'))
+    
+    ### calculating pvalue for each data point in  
+    basline_df$pval = sapply(1:nrow(basline_df), function(i) 
+      sum(shuffle_df$importance>basline_df$importance[i])/nrow(shuffle_df))
+    
+    row_name = paste0(residual_type, '-', model_type)
+    print(paste0(rep('----',20),collapse = ''))
+    print(row_name)
+    print((head(basline_df, 3)))
+    print(summary(basline_df$pval))
+    
+    #### defining which elements in baseline are considered as significant based on the threshold
+    sapply(1:length(basline_df), function(i) {basline_df$sig <<- basline_df$pval < thr})
+    
+    a_model_imp_df_cov = split(basline_df, basline_df$covariate_level)
+    AvgFacSig_per_cov = sapply(1:length(a_model_imp_df_cov), function(i){
+      sum(a_model_imp_df_cov[[i]]$sig)
+    })
+    names(AvgFacSig_per_cov) = names(a_model_imp_df_cov)
+    summary_df[row,]=AvgFacSig_per_cov
+    rownames(summary_df)[row]=row_name
+    row=row+1
+    
+      
+  }
+}
+
+
+summary_df_m_imp = melt( t(summary_df))
+head(summary_df_m_imp)
+ggplot(summary_df_m_imp, aes(y=value,x=Var2))+geom_boxplot()+
+  theme_classic()+scale_fill_brewer(palette = 'Set1')+
+  coord_flip()+theme(text = element_text(size=17))+xlab('')+
+  ylab('Average #sig matched factors per covariate level')+
+  geom_hline(yintercept=1, color = "red", size=1, linetype="dashed")+
+  ggtitle(paste0('pvalue threshold=',thr))
+
+
+
 
 ###############################################################################################
 ########################## importance evaluation for model comparison
@@ -81,7 +160,7 @@ meanimp_df_merged_response$res = 'response'
 meanimp_df_merged_deviance = read.csv('~/sciFA/Results/benchmark/deviance//base/meanimp_df_scMixology_deviance_baseline.csv')
 meanimp_df_merged_deviance$res = 'deviance'
 
-meanimp_df_residual_baseline = rbind(rbind(meanimp_df_merged_response, meanimp_df_merged_pearson))#meanimp_df_merged_deviance
+meanimp_df_residual_baseline = rbind(rbind(meanimp_df_merged_response, meanimp_df_merged_pearson)) #meanimp_df_merged_deviance
 head(meanimp_df_residual_baseline)
 meanimp_df_residual_baseline$type = 'baseline'
 
@@ -114,86 +193,13 @@ head(meanimp_residual_merged)
 meanimp_residual_merged_m = melt(meanimp_residual_merged)
 head(meanimp_residual_merged_m)
 
-
-
 ggplot2::ggplot(meanimp_residual_merged_m, aes(y=value, x=res, fill=type))+geom_boxplot()+
   theme_classic()+coord_flip()+ylab('Mean importance value')
 
-
+###################### Comparing mean importance benchmark - residuals,mean,scale comparison 
 
 meanimp_df_residual_baseline_m = melt(meanimp_df_residual_baseline)
-head(meanimp_df_residual_baseline_m)
 meanimp_df_residual_shuffle_m = melt(meanimp_residual_shuffle)
-head(meanimp_df_residual_shuffle_m)
-
-
-
-###################### Comparing residuals - ALL included
-mean_imp_baseline_m = data.frame(cov_level=meanimp_df_residual_baseline_m$X, 
-                                 factor=meanimp_df_residual_baseline_m$variable,
-                                 imp_score=meanimp_df_residual_baseline_m$value,
-                                 res=meanimp_df_residual_baseline_m$res)
-head(mean_imp_baseline_m)
-
-
-
-importance_df_shuffle_split <- split(meanimp_df_residual_shuffle_m, meanimp_df_residual_shuffle_m$res)
-lapply(importance_df_shuffle_split, head)
-importance_df_baseline_split <- split(meanimp_df_residual_baseline_m, meanimp_df_residual_baseline_m$res)
-lapply(importance_df_baseline_split, head)
-
-names(importance_df_shuffle_split)
-names(importance_df_baseline_split)
-
-for(i in 1:length(importance_df_shuffle_split)){
-  a_importance_df_shuffle = importance_df_shuffle_split[[i]]
-  a_importance_df_basline = importance_df_baseline_split[[i]]
-  importance_df_baseline_split[[i]]$pval = sapply(1:nrow(a_importance_df_basline), function(i) 
-    sum(a_importance_df_shuffle$value>a_importance_df_basline$value[i])/nrow(a_importance_df_shuffle))
-}
-
-tab=rbind(pval_0.05=data.frame(lapply(importance_df_baseline_split, function(x) sum(x$pval<0.05))),
-          pval_0.01=data.frame(lapply(importance_df_baseline_split, function(x) sum(x$pval<0.01))),
-          pval_0.001=data.frame(lapply(importance_df_baseline_split, function(x) sum(x$pval<0.001))))
-
-gridExtra::grid.table(t(tab))
-dev.off()
-
-tab=rbind(pval_0.05=data.frame(lapply(importance_df_baseline_split, function(x) round(sum(x$pval<0.05)/180,2))),
-          pval_0.01=data.frame(lapply(importance_df_baseline_split, function(x) round(sum(x$pval<0.01)/180,2))),
-          pval_0.001=data.frame(lapply(importance_df_baseline_split, function(x) round(sum(x$pval<0.001)/180,2))))
-gridExtra::grid.table(t(tab))
-
-thr = 0.01
-sapply(1:length(importance_df_baseline_split), function(i) 
-{importance_df_baseline_split[[i]]$sig <<- importance_df_baseline_split[[i]]$pval < thr})
-head(importance_df_baseline_split[[1]])
-
-AvgFacSig_df_model = sapply(1:length(importance_df_baseline_split), function(i){
-  a_model_imp_df = importance_df_baseline_split[[i]]
-  a_model_imp_df_cov = split(a_model_imp_df, a_model_imp_df$X)
-  AvgFacSig = sapply(1:length(a_model_imp_df_cov), function(i){
-    sum(a_model_imp_df_cov[[i]]$sig)
-  })
-  names(AvgFacSig) = names(a_model_imp_df_cov)
-  return(AvgFacSig)
-}, simplify = T)
-
-colnames(AvgFacSig_df_model) = names(importance_df_baseline_split) 
-head(AvgFacSig_df_model)
-AvgFacSig_df_model_m = melt(AvgFacSig_df_model)
-head(AvgFacSig_df_model_m)
-
-ggplot(AvgFacSig_df_model_m, aes(y=value,x=Var2))+geom_boxplot()+
-  theme_classic()+scale_fill_brewer(palette = 'Set1')+
-  coord_flip()+theme(text = element_text(size=17))+xlab('')+
-  ylab('Average #sig matched factors per covariate level')+
-  geom_hline(yintercept=1, color = "red", size=1, linetype="dashed")+
-  ggtitle(paste0('pvalue threshold=',thr))
-
-
-###################### Comparing residuals - ALL included
-
 head(meanimp_df_residual_baseline_m)
 head(meanimp_df_residual_shuffle_m)
 
@@ -205,7 +211,7 @@ table(meanimp_df_residual_baseline_m$scale_type)
 residual_type_names = names(table(meanimp_df_residual_baseline_m$residual_type))
 mean_type_names = names(table(meanimp_df_residual_baseline_m$mean_type))
 scale_type_names = names(table(meanimp_df_residual_baseline_m$scale_type))
- 
+
 for (residual_type in residual_type_names){
   res_df = meanimp_df_residual_baseline_m[meanimp_df_residual_baseline_m$residual_type==residual_type,]
   for (mean_type in mean_type_names){
@@ -214,6 +220,7 @@ for (residual_type in residual_type_names){
       res_df_mean_scale = res_df_mean[res_df_mean$scale_type==scale_type,]
       print(head(res_df_mean_scale, 20))
       print(paste0(residual_type, '_', mean_type, '_', scale_type,'.csv'))
+      #write.csv(res_df_mean_scale,paste0('~/sciFA/Results/benchmark/analysis/baseline_df_',residual_type, '_', mean_type, '_', scale_type,'.csv'))
     }
   }
 }
@@ -226,9 +233,6 @@ table(meanimp_df_residual_shuffle_m$residual_type)
 table(meanimp_df_residual_shuffle_m$mean_type)
 table(meanimp_df_residual_shuffle_m$scale_type)
 
-residual_type_names = names(table(meanimp_df_residual_shuffle_m$residual_type))
-mean_type_names = names(table(meanimp_df_residual_shuffle_m$mean_type))
-scale_type_names = names(table(meanimp_df_residual_shuffle_m$scale_type))
 
 for (residual_type in residual_type_names){
   res_df = meanimp_df_residual_shuffle_m[meanimp_df_residual_shuffle_m$residual_type==residual_type,]
@@ -239,9 +243,71 @@ for (residual_type in residual_type_names){
       print(dim(res_df_mean_scale))
       print(head(res_df_mean_scale, 3))
       print(paste0(residual_type, '_', mean_type, '_', scale_type,'.csv'))
+      write.csv(res_df_mean_scale,paste0('~/sciFA/Results/benchmark/analysis/shuffle_df_',residual_type, '_', mean_type, '_', scale_type,'.csv'))
     }
   }
 }
+
+
+
+thr = 0.05
+summary_df = data.frame(matrix(nrow = length(residual_type_names)*length(mean_type_names)*length(scale_type_names),
+                  ncol = 6))
+colnames(summary_df) = c("b'CELseq2'", "b'Dropseq'", "b'sc_10X'",  "H1975","H2228","HCC827" )
+
+row=1
+for (residual_type in residual_type_names){
+  for (mean_type in mean_type_names){
+    for (scale_type in scale_type_names){
+      basline_df = read.csv(paste0('~/sciFA/Results/benchmark/analysis/meanimp/baseline_df_',
+                                   residual_type, '_', mean_type, '_', scale_type,'.csv'))
+      shuffle_df = read.csv(paste0('~/sciFA/Results/benchmark/analysis/meanimp/shuffle_df_',
+                                   residual_type, '_', mean_type, '_', scale_type,'.csv'))
+      ### calculating pvalue for each data point in  
+      basline_df$pval = sapply(1:nrow(basline_df), function(i) 
+        sum(shuffle_df$value>basline_df$value[i])/nrow(shuffle_df))
+      
+      row_name = paste0(residual_type, '-', mean_type, '-', scale_type)
+      print(paste0(rep('----',20),collapse = ''))
+      print(row_name)
+      print((head(basline_df, 3)))
+      print(summary(basline_df$pval))
+      
+      #### defining which elements in baseline are considered as significant based on the threshold
+      sapply(1:length(basline_df), function(i) {basline_df$sig <<- basline_df$pval < thr})
+      
+      a_model_imp_df_cov = split(basline_df, basline_df$X)
+      AvgFacSig_per_cov = sapply(1:length(a_model_imp_df_cov), function(i){
+          sum(a_model_imp_df_cov[[i]]$sig)
+        })
+      names(AvgFacSig_per_cov) = names(a_model_imp_df_cov)
+      summary_df[row,]=AvgFacSig_per_cov
+      rownames(summary_df)[row]=row_name
+      row=row+1
+      
+
+    }
+  }
+}
+
+
+summary_df_m_meanimp = melt( t(summary_df))
+ggplot(summary_df_m_meanimp, aes(y=value,x=Var2))+geom_boxplot()+
+  theme_classic()+scale_fill_brewer(palette = 'Set1')+
+  coord_flip()+theme(text = element_text(size=17))+xlab('')+
+  ylab('Average #sig matched factors per covariate level')+
+  geom_hline(yintercept=1, color = "red", size=1, linetype="dashed")+
+  ggtitle(paste0('pvalue threshold=',thr))
+
+
+
+summary_df_m_both = rbind(summary_df_m_meanimp, summary_df_m_imp)
+ggplot(summary_df_m_both, aes(y=value,x=Var2))+geom_boxplot()+
+  theme_classic()+scale_fill_brewer(palette = 'Set1')+
+  coord_flip()+theme(text = element_text(size=17))+xlab('')+
+  ylab('Average #sig matched factors per covariate level')+
+  geom_hline(yintercept=1, color = "red", size=1, linetype="dashed")+
+  ggtitle(paste0('pvalue threshold=',thr))
 
 
 
